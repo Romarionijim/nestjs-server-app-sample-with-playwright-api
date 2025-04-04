d9f270a0a0accbdbc28a5301f10bdccd93d6c236
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-04 17:18:53 +0300

    feat: add Swagger documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Small Server App')
+    .setDescription('Small Server App API description')
+    .setVersion('1.0')
+    .addTag('small-server-app')
+    .build();
+  const document = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
   await app.listen(port);
 }
+
 bootstrap();
