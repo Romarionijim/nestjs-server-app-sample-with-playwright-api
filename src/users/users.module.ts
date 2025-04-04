2402624a045031a5684fdf44bbdf6e5d55ea1666
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-04 19:04:58 +0300

    feat: add JwtModule import to users module for authentication support

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService]
