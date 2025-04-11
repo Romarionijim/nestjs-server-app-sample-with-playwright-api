07cf39af247b6be56880f801fdf4c67f9aab05c3
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-11 13:35:08 +0300

    feat: update TypeScript configuration and add new test commands for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,15 +4,14 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './test-automation/tests',
+  testDir: './tests/api-tests/',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
