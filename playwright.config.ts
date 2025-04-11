88d84a2cf9238a86c4368e035522bb4498f2e6db
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-11 15:51:43 +0300

    chore: update .gitignore and Playwright config to include results files

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,12 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html', { open: 'never', outputFolder: './api/playwright-report/' }],
+    ['list'],
+    ['json', { outputFile: './api/results.json' }],
+    ['junit', { outputFile: './api/results.xml' }]
+  ],
   use: {
     trace: 'on-first-retry',
   },
