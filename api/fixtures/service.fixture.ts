b4748d261c1237ad794cbdbb2c903b2695a276ff
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-15 20:36:38 +0300

    feat: add authentication and service fixtures for Playwright tests

---
+++ api/fixtures/service.fixture.ts
@@ -0,0 +1,18 @@
+import { AuthService } from "api/infra/services/auth/auth.service";
+import { UsersService } from "api/infra/services/users/user.service"
+import { test as base } from '@playwright/test';
+
+type TestFixtures = {
+  usersService: UsersService;
+  authService: AuthService;
+}
+
+export const serviceFixtures = base.extend<TestFixtures>({
+  usersService: async ({ request }, use) => {
+    await use(new UsersService(request))
+  },
+  authService: async ({ request }, use) => {
+    await use(new AuthService(request))
+  }
+})
+
