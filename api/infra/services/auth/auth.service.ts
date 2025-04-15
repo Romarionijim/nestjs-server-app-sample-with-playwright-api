b560f16455142b5affda17729746d2d778c35dc9
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-15 20:36:54 +0300

    feat: refactor authentication service and add comprehensive API tests

--- api/infra/services/auth/auth.service.ts
+++ api/infra/services/auth/auth.service.ts
@@ -11,25 +11,14 @@ export class AuthService extends ApiClient {
   }
 
   async register(data: User) {
-    return await this.post(EndPoint.REGISTER, { data });
+    return await this.post(`${EndPoint.AUTH}/${EndPoint.REGISTER}`, { data });
   }
 
   async login(credentials: { username: string, password: string }) {
-    return await this.post(EndPoint.LOGIN, { data: credentials });
+    return await this.post(`${EndPoint.AUTH}/${EndPoint.LOGIN}`, { data: credentials });
   }
 
-  async getProfile(credentials: { username?: string, password?: string } = {}) {
-    const isAuthenticated = await this.isAuthenticated();
-    if (!isAuthenticated && credentials.username && credentials.password) {
-      const response = await this.login(
-        {
-          username: credentials.username,
-          password: credentials.password
-        }
-      );
-      const { access_token } = await response.json();
-      await this.setToken(access_token);
-    }
-    return await this.get(EndPoint.PROFILE);
+  async getProfile() {
+    return await this.get(`${EndPoint.AUTH}/${EndPoint.PROFILE}`, { isAuthRequired: true });
   }
 }
---
+++ api/tests/auth-api/authentication-tests.spec.ts
@@ -0,0 +1,65 @@
+import { expect } from '@playwright/test';
+import { test } from '@api-infra';
+import { StatusCode } from 'api/enums/status-codes.enum';
+import { TestTags } from 'api/enums/test-tags.enum';
+import { AuthService } from 'api/infra/services/auth/auth.service';
+import { logger } from 'api/logger/custom.logger';
+import { User } from 'api/types/user/user.types';
+import { MockData } from 'api/utils/mocks/mocks';
+
+test.describe('Authentication api tests', { tag: TestTags.AUTH }, async () => {
+  // let authService: AuthService;
+  // let mockData: MockData;
+  // let mockUser: User;
+  // let accessToken: string;
+  // let sharedUsername: string;
+  // let sharedPassword: string;
+
+  // test.beforeEach(async ({ request }) => {
+  //   authService = new AuthService(request);
+  //   mockData = new MockData();
+  //   mockUser = mockData.generateMockUser();
+  // })
+  let adminUser: User;
+
+  test.beforeEach(async ({ mockUser }) => {
+    adminUser = {
+      ...mockUser,
+      roles: ['admin']
+    }
+  })
+  test('should register user and login user', async ({ authenticationService, mockUser }) => {
+
+    await test.step('should register and create user successfully - [POST] /auth/register', async () => {
+      const response = await authenticationService.register(adminUser);
+      const responseBody = await response.json();
+      await expect(response).toBeOK();
+      expect(response.status()).toBe(StatusCode.CREATED);
+      expect(responseBody.access_token).toBeDefined();
+      expect(responseBody.access_token).toBeTruthy();
+      expect(responseBody.message).toBe('registered in successfully!');
+      expect(responseBody.name).toBe(adminUser.name);
+      expect(responseBody.lastName).toBe(adminUser.lastName);
+    });
+
+    await test.step('should login with created credentials successfully - [POST] /auth/login', async () => {
+      const response = await authenticationService.login({
+        username: adminUser.username,
+        password: adminUser.password
+      });
+      const responseBody = await response.json();
+      await expect(response).toBeOK();
+      expect(responseBody.access_token).toBeDefined();
+      expect(responseBody.access_token).toBeTruthy();
+      expect(responseBody.message).toBe('logged in successfully!');
+    })
+
+    await test.step('should navigate to my profile authenticated as an admin - [GET] /auth/profile', async () => {
+      const response = await authenticationService.getProfile();
+      const responseBody = await response.json();
+      expect(responseBody.roles).toStrictEqual(['admin']);
+      expect(responseBody.iat).toBeTruthy(); //the time when thr jwt was issued
+      expect(responseBody.exp).toBeTruthy(); //expra
+    });
+  });
+})
