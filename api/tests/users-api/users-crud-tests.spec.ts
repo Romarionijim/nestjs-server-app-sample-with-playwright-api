a9044ab68dbf5b3ec527d8b7fed6df6b4af7b810
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-15 10:12:37 +0300

    refactor: replace in-memory user data with MockData class and update user role generation

--- api/tests/users-api/users-crud-tests.spec.ts
+++ api/tests/users-api/users-crud-tests.spec.ts
@@ -1,64 +1,25 @@
 import { test, expect } from '@playwright/test';
-import { AuthService, TestTags, StatusCode, UsersService } from '@api-infra';
+import { AuthService, TestTags, StatusCode, UsersService, MockData } from '@api-infra';
+import { usersTestData } from './users-crud-test-data';
 
 test.describe('Users entity API CRUD tests - [GET, POST, PUT, DELETE] /users', { tag: TestTags.USERS }, async () => {
   let usersService: UsersService;
   let authService: AuthService;
-  const inMemoryData = [
-    {
-      id: 1,
-      name: 'John',
-      lastName: 'Doe',
-      username: 'johndoe',
-      password: 'secret',
-      hobbie: 'Basketball',
-      gender: 'male',
-      roles: ['admin']
-    },
-    {
-      id: 2,
-      name: 'Jane',
-      lastName: 'Smith',
-      username: 'janesmith',
-      password: 'secret',
-      hobbie: 'Tennis',
-      gender: 'female',
-      roles: ['user']
-    },
-    {
-      id: 3,
-      name: 'Sara',
-      lastName: 'James',
-      username: 'sarajames',
-      password: 'secret',
-      hobbie: 'Volleyball',
-      gender: 'female',
-      roles: ['user']
-    },
-    {
-      id: 4,
-      name: 'James',
-      lastName: 'Jones',
-      username: 'jamesjones',
-      password: 'secret',
-      hobbie: 'Football',
-      gender: 'male',
-      roles: ['user']
-    },
-  ]
+  let mockData: MockData;
 
   test.beforeEach(async ({ request }) => {
     usersService = new UsersService(request);
     authService = new AuthService(request);
+    mockData = new MockData();
   })
-
+  
   test('should get all users - [GET] /users', async () => {
     const response = await usersService.getAllUsers();
     const responseObj = await response.json();
     await expect(response).toBeOK();
     expect(response.status()).toBe(StatusCode.OK);
     expect(responseObj).toBeDefined();
-    expect(responseObj).toStrictEqual(inMemoryData);
+    expect(responseObj).toStrictEqual(usersTestData);
   })
 
   test('should get user by id - [GET] /users/:id', async () => {
@@ -198,4 +159,9 @@ test.describe('Users entity API CRUD tests - [GET, POST, PUT, DELETE] /users', {
       expect(userData[0].id).toBe(4);
     });
   });
+
+
+  test('create new user - [POST] /users', async() => {
+    //
+  })
 })
--- api/utils/mocks/mocks.ts
+++ api/utils/mocks/mocks.ts
@@ -2,13 +2,13 @@ import { faker } from '@faker-js/faker';
 
 export class MockData {
   
-  generateMockUser(gender: 'male' | 'female') {
+  generateMockUser(gender: 'male' | 'female' = 'female') {
     const name = faker.person.firstName(gender);
     const lastName = faker.person.lastName();
     const username = `${name.toLowerCase()}${lastName.toLowerCase()}`;
     const password = faker.internet.password();
     const hobbie = this.generateSport();
-    const roles = ['user'];
+    const roles = ['admin'];
 
     return {
       name,
