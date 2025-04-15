d1af818fc4388907cf5d342d8b25578f1f699b28
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-15 11:56:53 +0300

    refactor: simplify AuthService and UsersService constructors by removing request context

--- api/infra/services/users/user.service.ts
+++ api/infra/services/users/user.service.ts
@@ -1,38 +1,39 @@
 import { ApiClient } from "api/infra/api-client";
-import { APIRequestContext } from "@playwright/test";
 import { BaseUrl } from "api/enums/application-urls.enum";
 import { EndPoint } from "api/enums/endpoints.enum";
 import { User } from "api/types/user/user.types";
 
-export class UsersService extends ApiClient {
-  constructor(request: APIRequestContext) {
-    super(request, BaseUrl.LOCAL_HOST)
+export class UsersService {
+  private apiClient: ApiClient;
+
+  constructor() {
+    this.apiClient = new ApiClient(BaseUrl.LOCAL_HOST);
   }
 
   async getAllUsers(queryParams?: { [key: string]: string | number | boolean; }) {
-    return await this.get(EndPoint.USERS, { queryParams });
+    return await this.apiClient.get(EndPoint.USERS, { queryParams });
   }
 
   async getUser(id: number) {
-    return await this.get(`${EndPoint.USERS}/${id}`);
+    return await this.apiClient.get(`${EndPoint.USERS}/${id}`);
   }
 
   async createUser(user: User) {
-    return await this.post(EndPoint.USERS, {
+    return await this.apiClient.post(EndPoint.USERS, {
       data: user,
       isAuthRequired: true
     });
   }
 
   async updateUser(id: number, updatedFields: User) {
-    return await this.put(`${EndPoint.USERS}/${id}`, {
+    return await this.apiClient.put(`${EndPoint.USERS}/${id}`, {
       data: updatedFields,
       isAuthRequired: true
     });
   }
 
   async deleteUser(id: number) {
-    return await this.delete(`${EndPoint.USERS}/${id}`, {
+    return await this.apiClient.delete(`${EndPoint.USERS}/${id}`, {
       isAuthRequired: true
     });
   }
