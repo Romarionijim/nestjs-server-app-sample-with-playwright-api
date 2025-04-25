79db57f641e210c5f7e314865ac3c1e37649bb58
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-04-25 15:02:45 +0300

    feat: enhance user service with authentication handling for create, update, and delete operations

--- api/infra/services/users/user.service.ts
+++ api/infra/services/users/user.service.ts
@@ -3,37 +3,100 @@ import { APIRequestContext } from "@playwright/test";
 import { BaseUrl } from "api/enums/application-urls.enum";
 import { EndPoint } from "api/enums/endpoints.enum";
 import { User } from "api/types/user/user.types";
+import { AuthService } from "../auth/auth.service";
 
-export class UsersService extends ApiClient {
+export class UsersService {
+  apiClient: ApiClient;
+  authService: AuthService
+
   constructor(request: APIRequestContext) {
-    super(request, BaseUrl.LOCAL_HOST)
+    this.apiClient = new ApiClient(request, BaseUrl.LOCAL_HOST);
+    this.authService = new AuthService(request);
   }
 
   async getAllUsers(queryParams?: { [key: string]: string | number | boolean; }) {
-    return await this.get(EndPoint.USERS, { queryParams });
+    return await this.apiClient.get(EndPoint.USERS, { queryParams });
   }
 
   async getUser(id: number) {
-    return await this.get(`${EndPoint.USERS}/${id}`);
+    return await this.apiClient.get(`${EndPoint.USERS}/${id}`);
   }
 
-  async createUser(user: User) {
-    return await this.post(EndPoint.USERS, {
+  async createUser(
+    user: User,
+    {
+      username = process.env.ADMIN_USER as string,
+      password = process.env.ADMIN_PASSWORD as string,
+    }: {
+      username?: string;
+      password?: string;
+    } = {}
+  ) {
+    const token = await this.apiClient.getToken();
+
+    if (!token) {
+      const { access_token } = await this.authService.login({
+        username: username,
+        password: password
+      })
+      await this.apiClient.setToken(access_token)
+    }
+
+    return await this.apiClient.post(EndPoint.USERS, {
       data: user,
       isAuthRequired: true
     });
   }
 
   async updateUser(id: number, updatedFields: User) {
-    return await this.put(`${EndPoint.USERS}/${id}`, {
+    const token = await this.apiClient.getToken();
+
+    if (!token) {
+      const { access_token } = await this.authService.login({
+        username: updatedFields.username,
+        password: updatedFields.password
+      })
+      await this.apiClient.setToken(access_token)
+    }
+
+    return await this.apiClient.put(`${EndPoint.USERS}/${id}`, {
       data: updatedFields,
       isAuthRequired: true
     });
   }
 
-  async deleteUser(id: number) {
-    return await this.delete(`${EndPoint.USERS}/${id}`, {
+  async deleteUser(id: number, admin: {
+    username?: string,
+    password?: string
+  } = {}
+  ) {
+    const token = await this.apiClient.getToken();
+
+    if (!token) {
+      const { access_token } = await this.authService.login({
+        username: admin.username!,
+        password: admin.password!
+      })
+      await this.apiClient.setToken(access_token)
+    }
+
+    return await this.apiClient.delete(`${EndPoint.USERS}/${id}`, {
       isAuthRequired: true
     });
+  }
+
+  private async ensureAuthenticatd(admin: {
+    username?: string,
+    password?: string
+  } = {}
+  ) {
+    const username = admin.username || process.env.ADMIN_USER as string;
+    const password = admin.password || process.env.ADMIN_PASSWORD as string;
+
+    const token = await this.apiClient.getToken();
+    if (!token) {
+      const { access_token } = await this.authService.login({ username, password })
+      await this.apiClient.setToken(access_token);
+    }
   }
 }
