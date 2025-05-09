aa75f9883d21ae19e9e911d6357cc579ca580117
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2025-05-09 10:32:52 +0300

    refactor: remove duplicated file

--- api/fixtures/all-fixtures.fixture.ts
+++
@@ -1,8 +0,0 @@
-import { mergeTests as mergeMultipleFixtures } from "@playwright/test";
-import { authFixtures } from "./auth.fixture";
-import { serviceFixtures } from "./service.fixture";
-
-export const test = mergeMultipleFixtures(
-  authFixtures,
-  serviceFixtures
-)
